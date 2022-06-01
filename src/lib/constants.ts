528685a2882925e9fa04833bb97397d127bd5d88
TimJ <timothyneiljohnson@gmail.com>
2022-06-01 00:19:12 -0500

    Reference env variable

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,10 @@
-export const BASE_URL = 'http://localhost:10003'; // TODO: Move to env variable?
+const { API_BASE_URL } = process.env;
 export const FEATURED_CATEGORY_ID = 3;
 const BANNER_CATEGORY_ID = 9;
-export const POSTS_API_URL = `${BASE_URL}/wp-json/wp/v2/posts`;
-export const POSTS_WITH_IMAGES_API_URL = `${BASE_URL}/wp-json/wp/v2/posts?_embed`;
-export const FEATURED_CATEGORIES_API_URL = `${BASE_URL}/wp-json/wp/v2/categories?parent=${FEATURED_CATEGORY_ID}`;
-export const BANNER_POSTS_API_URL = `${BASE_URL}/wp-json/wp/v2/posts?categories=${BANNER_CATEGORY_ID}&_embed&acf_format=standard`;
-export const FEATURED_POSTS_API_URL = `${BASE_URL}/wp-json/wp/v2/posts?categories=${FEATURED_CATEGORY_ID}&_embed&acf_format=standard`;
-export const MEDIA_API_URL = `${BASE_URL}/wp-json/wp/v2/media`;
-export const PAGES_API_URL = `${BASE_URL}/wp-json/wp/v2/pages`;
+export const POSTS_API_URL = `${API_BASE_URL}/wp-json/wp/v2/posts`;
+export const POSTS_WITH_IMAGES_API_URL = `${API_BASE_URL}/wp-json/wp/v2/posts?_embed`;
+export const FEATURED_CATEGORIES_API_URL = `${API_BASE_URL}/wp-json/wp/v2/categories?parent=${FEATURED_CATEGORY_ID}`;
+export const BANNER_POSTS_API_URL = `${API_BASE_URL}/wp-json/wp/v2/posts?categories=${BANNER_CATEGORY_ID}&_embed&acf_format=standard`;
+export const FEATURED_POSTS_API_URL = `${API_BASE_URL}/wp-json/wp/v2/posts?categories=${FEATURED_CATEGORY_ID}&_embed&acf_format=standard`;
+export const MEDIA_API_URL = `${API_BASE_URL}/wp-json/wp/v2/media`;
+export const PAGES_API_URL = `${API_BASE_URL}/wp-json/wp/v2/pages`;
